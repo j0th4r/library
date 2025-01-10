5331a54d26cb2e424f04b8c303193dbb7f8052fb
j0th4r <124442886+j0th4r@users.noreply.github.com>
2025-01-10 22:19:25 +0800

    Refactor codebase to use class instead of plain constructors

--- index.js
+++ index.js
@@ -1,77 +1,86 @@
-const myLibrary = [];
 
-function Book(author, title, pages, read) {
-  this.author = author;
-  this.title = title;
-  this.pages = pages;
-  this.read = read;
-}
 
-Book.prototype.toggleRead = function() {
-  this.read = !this.read;
-}
+class Book {
+  constructor(author, title, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+  }
 
-function toggleRead(index) {
-  myLibrary[index].toggleRead();
-  render();
-}
-
-function addBookToLibrary() {
-  // do stuff here
-  let author = document.querySelector("#author").value;
-  let title = document.querySelector("#title").value;
-  let pages = document.querySelector("#pages").value;
-  let read = document.querySelector("#read").checked;
-
-  let newBook = new Book(author, title, pages, read);
-  myLibrary.push(newBook);
-  render();
-}
-
-function render() {
-  const bookList = document.querySelector("#book-list");
-  bookList.innerHTML = "";
-  for (let i = 0; i < myLibrary.length; i++) {
-    let book = myLibrary[i];
-    let bookEl = document.createElement("div");
-    bookEl.setAttribute("class", "book-card");
-    bookEl.innerHTML = `
-      <div class="card-header">
-        <h3 class="title">${book.title}</h3>
-        <h5 class="author">${book.author}</h5>
-      </div>
-      <div class="card-body">
-        <p>${book.pages} pages</p>
-        <button class="read-btn" onClick="toggleRead(${i})">${book.read ? "Read" : "Not Read"}</button>
-      </div>
-      <hr>
-      <button class="remove-btn" onClick="removeBook(${i})">Remove</button>
-    `;
-    bookList.appendChild(bookEl);
+  toggleRead() {
+    this.read = !this.read;
   }
 }
 
-function removeBook(index) {
-  myLibrary.splice(index, 1);
-  render();
+class Library {
+  constructor() {
+    this.books = [];
+    this.eventListeners();
+  }
+
+  render() {
+    const bookList = document.querySelector("#book-list");
+    bookList.innerHTML = "";
+    for (let i = 0; i < this.books.length; i++) {
+      let book = this.books[i];
+      let bookEl = document.createElement("div");
+      bookEl.setAttribute("class", "book-card");
+      bookEl.innerHTML = `
+        <div class="card-header">
+          <h3 class="title">${book.title}</h3>
+          <h5 class="author">${book.author}</h5>
+        </div>
+        <div class="card-body">
+          <p>${book.pages} pages</p>
+          <button class="read-btn" onClick="library.toggleRead(${i})">${book.read ? "Read" : "Not Read"}</button>
+        </div>
+        <hr>
+        <button class="remove-btn" onClick="library.removeBook(${i})">Remove</button>
+      `;
+      bookList.appendChild(bookEl);
+    }
+  }
+
+  toggleRead(index) {
+    this.books[index].toggleRead();
+    this.render();
+  }
+  
+  addBook(author, title, pages, read) {
+    const newBook = new Book(author, title, pages, read);
+    this.books.push(newBook);
+    this.render();
+  }
+  
+  removeBook(index) {
+    this.books.splice(index, 1);
+    this.render();
+  }
+  
+  eventListeners() {
+    const dialog = document.querySelector("dialog");
+    document.querySelector("#new-book-btn").addEventListener("click", () => dialog.showModal());
+  
+    document.querySelector("#close-btn").addEventListener("click", (e) => {
+      e.preventDefault();
+      dialog.close();
+    });
+  
+    document.querySelector("#new-book-form").addEventListener("submit", (e) => {
+      e.preventDefault();
+      const author = document.querySelector("#author").value;
+      const title = document.querySelector("#title").value;
+      const pages = document.querySelector("#pages").value;
+      const read = document.querySelector("#read").checked;
+
+      this.addBook(author, title, pages, read);
+      dialog.close();
+    });
+  }
 }
 
+const library = new Library();
 
-//Dialog open and close buttons
-const dialog = document.querySelector("dialog");
-document.querySelector("#new-book-btn").addEventListener("click", () => {
-  console.log("clicked");
-  dialog.showModal();
-});
 
-document.querySelector("#close-btn").addEventListener("click", (e) => {
-  event.preventDefault();
-  dialog.close();
-});
 
-//Form submit
-document.querySelector("#new-book-form").addEventListener("submit", (e) => {
-  e.preventDefault();
-  addBookToLibrary();
-  dialog.close();
-});
